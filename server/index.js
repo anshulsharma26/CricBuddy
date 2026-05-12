ae8a1764f00c15ca21a13dd6ff7907c16a045698
Anshul Sharma <ianshulsharma26@gmail.com>
2026-05-12 21:20:22 +0530

    add a home route

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const homeRoutes = require('./routes/home');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const matchRoutes = require('./routes/matches');
 const chatRoutes = require('./routes/chat');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -41,6 +43,7 @@ app.use(async (req, res, next) => {
 });
 
 // Routes
+app.use('/', homeRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/matches', matchRoutes);
